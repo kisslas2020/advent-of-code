9a59706eae903f51603f282e052d473534729d0a
kisslas2020 <pinewood82@gmail.com>
2021-12-05 10:13:09 +0100

    Create class Vent with equals method

---
+++ src/main/java/Day5.java
@@ -0,0 +1,44 @@
+import java.util.Objects;
+
+public class Day5 {
+
+    private class Vent {
+
+        private int x;
+        private int y;
+
+        public Vent(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Vent vent = (Vent) o;
+            return x == vent.x && y == vent.y;
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(x, y);
+        }
+
+        public int getX() {
+            return x;
+        }
+
+        public void setX(int x) {
+            this.x = x;
+        }
+
+        public int getY() {
+            return y;
+        }
+
+        public void setY(int y) {
+            this.y = y;
+        }
+    }
+}

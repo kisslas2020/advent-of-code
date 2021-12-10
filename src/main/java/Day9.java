96fe29fffca91442cc58654e1a35b08df399f37f
kisslas2020 <pinewood82@gmail.com>
2021-12-10 15:12:25 +0100

    Update Day9.java with Part 2 solution: class Basin, getNeoghbors method

--- src/main/java/Day9.java
+++ src/main/java/Day9.java
@@ -1,9 +1,7 @@
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Day9 {
 
@@ -13,7 +11,62 @@ public class Day9 {
         int[][] map = loadMap("src/main/resources/day9.txt");
         findLowPoints(map);
         int risk = lowPoints.stream().mapToInt(lp -> lp.getValue() + 1).sum();
-        System.out.println(risk);
+        System.out.println("Total risk is:" + risk);
+        List<Basin> basins = lowPoints.stream().map(lp -> getNeighbors(lp, map, new Basin(lp))).collect(Collectors.toList());
+        basins.sort(new Comparator<Basin>() {
+            @Override
+            public int compare(Basin o1, Basin o2) {
+                return o2.getBasinPoints().size() - o1.getBasinPoints().size();
+            }
+        });
+        int b1 = basins.get(0).getBasinPoints().size() + 1;
+        int b2 = basins.get(1).getBasinPoints().size() + 1;
+        int b3 = basins.get(2).getBasinPoints().size() + 1;
+        System.out.println("The product of the three largest basin's size: " + b1 * b2 * b3);
+    }
+
+    private static Basin getNeighbors(Point point, int[][] map, Basin basin) {
+        int row = point.getX();
+        int col = point.getY();
+        int value = map[row][col];
+
+        if (row > 0) {
+            int newValue = map[row - 1][col];
+            if (newValue < 9 && newValue > value) {
+                Point newPoint = new Point(row - 1, col, newValue);
+                getNeighbors(newPoint, map, basin);
+                basin.addBasinPoint(newPoint);
+                point.addNeighbor(newPoint);
+            }
+        }
+        if (col < map[0].length - 1) {
+            int newValue = map[row][col + 1];
+            if (newValue < 9 && newValue > value) {
+                Point newPoint = new Point(row, col + 1, newValue);
+                getNeighbors(newPoint, map, basin);
+                basin.addBasinPoint(newPoint);
+                point.addNeighbor(newPoint);
+            }
+        }
+        if (row < map.length - 1) {
+            int newValue = map[row + 1][col];
+            if (newValue < 9 && newValue > value) {
+                Point newPoint = new Point(row + 1, col, newValue);
+                getNeighbors(newPoint, map, basin);
+                basin.addBasinPoint(newPoint);
+                point.addNeighbor(newPoint);
+            }
+        }
+        if (col > 0) {
+            int newValue = map[row][col - 1];
+            if (newValue < 9 && newValue > value) {
+                Point newPoint = new Point(row, col - 1, newValue);
+                getNeighbors(newPoint, map, basin);
+                basin.addBasinPoint(newPoint);
+                point.addNeighbor(newPoint);
+            }
+        }
+        return basin;
     }
 
     private static void findLowPoints(int[][] map) {
@@ -88,11 +141,16 @@ class Point {
     private final int x;
     private final int y;
     private final int value;
+    private List<Point> neighbors = new ArrayList<>();
 
     public Point(int x, int y, int value) {
         this.x = x;
         this.y = y;
         this.value = value;
+    }
+
+    public void addNeighbor(Point point) {
+        neighbors.add(point);
     }
 
     public int getX() {
@@ -105,5 +163,40 @@ class Point {
 
     public int getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Point point = (Point) o;
+        return x == point.x && y == point.y;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+}
+
+class Basin {
+
+    private final Point lowPoint;
+    private final Set<Point> basinPoints = new HashSet<>();
+
+    public Basin(Point lowPoint) {
+        this.lowPoint = lowPoint;
+    }
+
+    public void addBasinPoint(Point point) {
+        basinPoints.add(point);
+    }
+
+    public Point getLowPoint() {
+        return lowPoint;
+    }
+
+    public Set<Point> getBasinPoints() {
+        return basinPoints;
     }
 }

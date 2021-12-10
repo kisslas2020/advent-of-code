aabc9d50b46f1854c2955fb45d11621b4cec1e06
kisslas2020 <pinewood82@gmail.com>
2021-12-10 15:58:49 +0100

    Move static initalizer into method

--- src/main/java/Day10.java
+++ src/main/java/Day10.java
@@ -7,7 +7,16 @@ public class Day10 {
     private static Map<String, String> collection = new HashMap<>();
     private static Map<String, Integer> score = new HashMap<>();
 
-    {
+
+
+
+
+    public static void main(String[] args) {
+        int sum = partOne("src/main/resources/day10example.txt");
+        System.out.println("Total syntax error score: " + sum);
+    }
+
+    private static void init() {
         collection.put("(", ")");
         collection.put("[", "]");
         collection.put("{", "}");
@@ -16,21 +25,20 @@ public class Day10 {
         score.put("]", 57);
         score.put("}", 1197);
         score.put(">", 25137);
+        score.put("", 0);
     }
 
-    public static void main(String[] args) {
-        partOne("src/main/resources/day10example.txt");
-    }
-
-    private static void partOne(String path) {
+    private static int partOne(String path) {
+        int sum = 0;
         try (Scanner scanner = new Scanner(new File(path))) {
             while (scanner.hasNext()) {
                 String res = checkLine(scanner.nextLine());
-
+                sum += score.get(res);
             }
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + path);
         }
+        return sum;
     }
 
     private static String checkLine(String line) {
@@ -38,9 +46,10 @@ public class Day10 {
         String[] brackets = line.split("");
         Stack<String> stack = new Stack<>();
         for (String s : brackets) {
+            System.out.println(s);
             if (collection.containsKey(s)) {
                 stack.add(s);
-            } else if (!collection.get(stack.pop()).equals(s)){
+            } else if (stack.empty() || !collection.get(stack.pop()).equals(s)){
                 return s;
             }
         }

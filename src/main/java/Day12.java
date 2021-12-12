73ddc7493c2d2f5d5c714939a07a30550cdd1951
kisslas2020 <pinewood82@gmail.com>
2021-12-12 12:25:13 +0100

    Add findNextStep method recursive search for next possible step

--- src/main/java/Day12.java
+++ src/main/java/Day12.java
@@ -2,11 +2,36 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Day12 {
+    
+    private static final List<String> routes = new ArrayList<>();
+    private static final List<Cave> caves = new ArrayList<>();
+
+    public static void main(String[] args) {
+        
+    }
 
 
+    private static void findNextStep(Queue<Cave> route, Cave prev) {
+        for (String neighbor : prev.getNeighbors().keySet()) {
+            Cave cave = caves.stream().filter(c -> c.getName().equals(neighbor)).findFirst().get();
+            if (!canVisitCave()) {
+                continue;
+            }
+            route.offer(cave);
+            if (cave.getName().equals("end")) {
+                return;
+            }
+            findNextStep(route, cave);
+        }
+        return;
+    }
 
-    private void recordRoute(Queue<Cave> route) {
+    private static boolean canVisitCave() {
+    }
+
+    private static void recordRoute(Queue<Cave> route) {
         String routeString = route.stream().map(r -> r.getName()).collect(Collectors.joining(","));
+        routes.add(routeString);
     }
 }
 
@@ -31,5 +56,18 @@ class Cave {
 
     public Map<String, Boolean> getNeighbors() {
         return neighbors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Cave cave = (Cave) o;
+        return name.equals(cave.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name);
     }
 }

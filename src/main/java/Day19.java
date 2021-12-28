2ca5084229542b3231ac283325df9ab2bc7ba62d
kisslas2020 <pinewood82@gmail.com>
2021-12-28 11:41:43 +0100

    Correct rotate method and add part two solution

--- src/main/java/Day19.java
+++ src/main/java/Day19.java
@@ -6,11 +6,32 @@ import java.util.stream.Collectors;
 public class Day19 {
 
     private static List<WaterScanner> scanners = new ArrayList<>();
+    private static List<Position> positions = new ArrayList<>();
 
     public static void main(String[] args) {
         String path = "src/main/resources/day19.txt";
         loadScanners(path);
         searchScanners();
+        System.out.println(scanners.get(0).getBeacons().size());
+        System.out.println(calculateManhattanDistance());
+    }
+
+    private static int calculateManhattanDistance() {
+        int maxDistance = 0;
+        Position[] res = new Position[2];
+        for (int i = 0; i < positions.size() - 1; i++) {
+            for (int j = i + 1; j < positions.size(); j++) {
+                Position p1 = positions.get(i);
+                Position p2 = positions.get(j);
+                int d = Math.abs(p1.getX() - p2.getX()) + Math.abs(p1.getY() - p2.getY()) + Math.abs(p1.getZ() - p2.getZ());
+                if (d > maxDistance) {
+                    maxDistance = d;
+                    res[0] = p1;
+                    res[1] = p2;
+                }
+            }
+        }
+        return maxDistance;
     }
 
     private static void loadScanners(String path) {
@@ -28,6 +49,7 @@ public class Day19 {
                 }
                 wsc.addBeacon(new Beacon(new Position(line.split(","))));
             }
+            scanners.add(wsc);
         } catch (FileNotFoundException e) {
             System.out.println("File not found: " + path);
         }
@@ -35,12 +57,36 @@ public class Day19 {
 
     private static void searchScanners() {
         WaterScanner WSC = scanners.get(0);
-        for (int i = 1; i < scanners.size(); i++) {
-            WaterScanner w = scanners.get(i);
+        positions.add(new Position(0, 0, 0));
+        int i = 1;
+        while (scanners.size() > 1) {
+            System.out.println(scanners.size() + " / " + i);
+            if (i > scanners.size() - 1) {
+                i = 1;
+            }
+            WaterScanner w = scanners.get(i++);
             Position p = compareScanners(WSC, w);
             if (p != null) {
                 w.setPosition(p);
+                mergeScanner(WSC, w);
+                positions.add(p);
+                scanners.remove(w);
+                i = 1;
             }
+        }
+    }
+
+    private static void mergeScanner(WaterScanner WSC, WaterScanner w) {
+        for (Beacon beacon : w.getBeacons()) {
+            Position p = beacon.getPosition();
+            Position sc = w.getPosition();
+            int i = w.getPosition().getI();
+            Position pRotated = rotate(i, p.getX(), p.getY(), p.getZ());
+            int x = pRotated.getX() + sc.getX();
+            int y = pRotated.getY() + sc.getY();
+            int z = pRotated.getZ() + sc.getZ();
+            Position pAbsolute = new Position(x, y, z);
+            WSC.addBeacon(new Beacon(pAbsolute));
         }
     }
 
@@ -53,16 +99,16 @@ public class Day19 {
                 for (int i = 0; i < 24; i++) {
                     Position rotated = rotate(i, original.getX(), original.getY(), original.getZ());
                     Position diff = distance(pBEACON, rotated);
-                    if (diff.getX() == 68) {
-                        System.out.printf("X: %d, Y: %d, Z: %d", diff.getX(), diff.getY(), diff.getZ());
-                        System.out.println();
-                    }
+                    diff.setI(i);
                     Integer n = distances.getOrDefault(diff, 0);
+                    if (n == 11) {
+                        return diff;
+                    }
                     distances.put(diff, n + 1);
                 }
             }
         }
-        List<Integer> pos = distances.keySet().stream().filter(p -> distances.get(p) > 11).map(p -> distances.get(p)).collect(Collectors.toList());
+        Map<Position, Integer> m = distances.entrySet().stream().filter(e -> e.getValue() > 1).collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
         Position res = null;
         for (Position p : distances.keySet()) {
             if (distances.get(p) >= 12) {
@@ -77,51 +123,51 @@ public class Day19 {
             case 0:
                 return new Position(x, y, z);
             case 1:
-                return new Position(z, y, -x);
+                return new Position(-y, x, z);
             case 2:
-                return new Position(x, z, -y);
+                return new Position(-x, -y, z);
             case 3:
-                return new Position(-z, y, x);
+                return new Position(y, -x, z);
             case 4:
                 return new Position(-x, y, -z);
             case 5:
-                return new Position(y, -z, y);
+                return new Position(-y, -x, -z);
             case 6:
-                return new Position(y, -x, z);
+                return new Position(x, -y, -z);
             case 7:
-                return new Position(y, -z, -x);
+                return new Position(y, x, -z);
             case 8:
-                return new Position(z, -x, -y);
+                return new Position(z, y, -x);
             case 9:
-                return new Position(-y, x, -z);
-            case 10:
-                return new Position(-y, z, x);
-            case 11:
-                return new Position(-z, x, y);
-            case 12:
-                return new Position(-x, -y, z);
-            case 13:
-                return new Position(-z, -y, -x);
-            case 14:
-                return new Position(-x, -z, -y);
-            case 15:
-                return new Position(x, y, -z);
-            case 16:
-                return new Position(z, y, x);
-            case 17:
-                return new Position(x, z, y);
-            case 18:
-                return new Position(-y, x, z);
-            case 19:
                 return new Position(-y, z, -x);
-            case 20:
+            case 10:
+                return new Position(-z, -y, -x);
+            case 11:
+                return new Position(y, -z, -x);
+            case 12:
+                return new Position(-z, y, x);
+            case 13:
+                return new Position(-y, -z, x);
+            case 14:
+                return new Position(z, -y, x);
+            case 15:
+                return new Position(y, z, x);
+            case 16:
+                return new Position(x, z, -y);
+            case 17:
                 return new Position(-z, x, -y);
+            case 18:
+                return new Position(-x, -z, -y);
+            case 19:
+                return new Position(z, -x, -y);
+            case 20:
+                return new Position(x, -z, y);
             case 21:
-                return new Position(y, -x, -z);
+                return new Position(z, x, y);
             case 22:
-                return new Position(y, -z, x);
+                return new Position(-x, z, y);
             case 23:
-                return new Position(z, -x, y);
+                return new Position(-z, -x, y);
             default:
                 return null;
         }
@@ -210,11 +256,19 @@ class Position {
     private final int x;
     private final int y;
     private final int z;
+    private int i;
 
     public Position(int x, int y, int z) {
         this.x = x;
         this.y = y;
         this.z = z;
+    }
+
+    public Position(int x, int y, int z, int i) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+        this.i = i;
     }
 
     public Position(String[] line) {
@@ -231,6 +285,14 @@ class Position {
 
     public int getZ() {
         return z;
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i) {
+        this.i = i;
     }
 
     @Override
